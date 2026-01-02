1711c4af3c1a3bdf3976c8a7091ce50c7e56b355
Federico Bozzini <federico.bozzini@gmail.com>
2026-01-02 10:58:30 +0100

    Added tests for extension activation (#118)

---
+++ src/extension.test.ts
@@ -0,0 +1,59 @@
+import * as vscode from 'vscode';
+import { activate } from './extension';
+
+jest.mock('vscode');
+jest.mock('./util/logger');
+jest.mock('./topoCliVersionChecker', () => {
+    return {
+        TopoCliVersionChecker: jest.fn().mockImplementation(() => ({
+            checkTopoCliVersion: jest.fn(() => Promise.resolve(true)),
+        })),
+    };
+});
+
+describe('extension activation', () => {
+
+    beforeEach(() => {
+        jest.useFakeTimers();
+    });
+
+    afterEach(async () => {
+        try {
+            jest.runOnlyPendingTimers();
+        } finally {
+            jest.clearAllTimers();
+            jest.useRealTimers();
+            jest.resetAllMocks();
+        }
+    });
+
+    it('registers commands and prepares disposables', async () => {
+        const subscriptions: vscode.Disposable[] = [];
+        const extensionPath = '/fake/extension/path';
+        const environmentVariableCollection = {
+            prepend: jest.fn(),
+        };
+        const globalState = {
+            get: jest.fn(),
+        };
+        const workspaceState = {
+            get: jest.fn(),
+        };
+        const globalStorageUri = vscode.Uri.file('/fake/storage/path');
+        const context = {
+            subscriptions,
+            extensionPath,
+            environmentVariableCollection,
+            globalState,
+            workspaceState,
+            globalStorageUri,
+        } as unknown as vscode.ExtensionContext;
+        (vscode.commands.registerCommand as jest.Mock).mockImplementation(() => ({ dispose: jest.fn() }));
+        (vscode.window.showInformationMessage as jest.Mock).mockImplementation(() => undefined);
+
+        await activate(context);
+
+        expect(vscode.commands.registerCommand).toHaveBeenCalled();
+        expect(subscriptions.length).toBeGreaterThan(0);
+    });
+});
