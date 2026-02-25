998a1192bef7071307a47b024ff4a64ca1e69e52
Federico Bozzini <federico.bozzini@gmail.com>
2026-02-25 14:10:20 +0000

    Use lodash.isPlainObject instead of custom one (#166)
    Signed-off-by: federicobozzini <federico.bozzini@gmail.com>

--- src/util/isPlainObject.ts
+++ src/util/isPlainObject.ts
@@ -1,10 +1,5 @@
+import lodashIsPlainObject from 'lodash.isplainobject';
+
 export const isPlainObject = (
     value: unknown,
-): value is Record<string, unknown> => {
-    if (typeof value !== 'object' || value === null) {
-        return false;
-    }
-
-    const proto = Object.getPrototypeOf(value);
-    return proto === Object.prototype || proto === null;
-};
+): value is Record<string, unknown> => lodashIsPlainObject(value);
