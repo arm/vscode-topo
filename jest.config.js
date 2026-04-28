e76ef07b0cc3892da7c59395d4fad592ccc9aa8c
Federico Bozzini <federico.bozzini@arm.com>
2026-04-28 13:54:04 +0100

    feat: remove target dashboard
    Signed-off-by: Federico Bozzini <federico.bozzini@arm.com>

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,8 @@
 const commonConfig = {
-  moduleNameMapper: {
-    "\\.(css|less)$": "identity-obj-proxy"
-  },
   transform: {
     "^.+\\.(ts|tsx)$": ["@swc/jest", {
       jsc: {
-        parser: { syntax: "typescript", tsx: true },
-        transform: { react: { runtime: "automatic" } }
+        parser: { syntax: "typescript", tsx: true }
       }
     }]
   },
@@ -23,13 +19,6 @@ module.exports = {
       displayName: "unit",
       testEnvironment: "node",
       roots: ["<rootDir>/src"],
-    },
-    {
-      ...commonConfig,
-      displayName: "ui",
-      testEnvironment: "jsdom",
-      roots: ["<rootDir>/media"],
-      setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
     }
   ],
 };
