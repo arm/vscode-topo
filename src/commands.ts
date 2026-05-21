fb017c5ca6d3886e5e37943731fadaa88b4798b9
awphi <26072111+awphi@users.noreply.github.com>
2026-05-21 15:38:16 +0100

    refactor: move showOutput command into new register flow
    Signed-off-by: awphi <26072111+awphi@users.noreply.github.com>

--- src/commands.ts
+++ src/commands.ts
@@ -1,18 +1,21 @@
 import { HostController } from './controllers/hostController';
 import { PACKAGE_NAME } from './manifest';
 import * as vscode from 'vscode';
+import { logger } from './util/logger';
 
 function command(id: string): string {
     return `${PACKAGE_NAME}.${id}`;
 }
 
 export const refreshHostHealth = command('refreshHostHealth');
+export const showOutputCommand = command('showOutput');
 
 export function register(hostController: HostController): vscode.Disposable {
     const disposables = [
         vscode.commands.registerCommand(refreshHostHealth, () =>
             hostController.refreshHealth(),
         ),
+        vscode.commands.registerCommand(showOutputCommand, () => logger.show()),
     ];
 
     return {
