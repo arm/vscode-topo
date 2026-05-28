13b42c025ccb62fab2a76818f14f1c5656b983d8
Federico Bozzini <federico.bozzini@gmail.com>
2026-05-28 12:59:11 +0100

    refactor: wrap malformed target store data errors (#144)
    Signed-off-by: Federico Bozzini <federico.bozzini@arm.com>

--- src/errors/wrappedError.ts
+++ src/errors/wrappedError.ts
@@ -1,4 +1,4 @@
-export type WrappedErrorCode = 'DOCKER' | 'CLONE' | 'CLI';
+export type WrappedErrorCode = 'DOCKER' | 'CLONE' | 'CLI' | 'STORAGE';
 
 export type WrappedErrorLogLevel = 'Error' | 'Warning' | 'Info' | 'Debug';
 
