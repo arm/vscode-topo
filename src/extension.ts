abae2bf03be0e1708918e12b58766bea5e365f5d
adamwph <26072111+awphi@users.noreply.github.com>
2026-06-10 16:01:33 +0100

    feat!: remove health commands (#218)
    Signed-off-by: awphi <26072111+awphi@users.noreply.github.com>

--- src/extension.ts
+++ src/extension.ts
@@ -20,11 +20,8 @@ import { TargetDescriptionStore } from './target/targetDescriptionStore';
 import { FixIssue } from './actions/fixIssue';
 import { HostTreeView } from './views/hostTreeView';
 import { logger } from './util/logger';
-import { TargetHealth } from './actions/targetHealth';
-import { HostHealth } from './actions/hostHealth';
 import { HostModel } from './models/hostModel';
 import { HostController } from './controllers/hostController';
-import { TransientDocumentProvider } from './util/transientDocumentProvider';
 import { TargetController } from './controllers/targetController';
 import { TargetModel } from './models/targetModel';
 import { ProjectClone } from './actions/projectClone';
@@ -48,18 +45,10 @@ export async function activate(
         return;
     }
 
-    const targetHealthDocProvider = new TransientDocumentProvider(
-        'target-health',
-    );
-    const hostHealthDocProvider = new TransientDocumentProvider('host-health');
     const dockerCommands = new DockerCommands();
     const targetStore = new TargetStore(context);
     const targetDescriptionStore = new TargetDescriptionStore(topoCli);
-    context.subscriptions.push(
-        targetStore,
-        hostHealthDocProvider,
-        targetHealthDocProvider,
-    );
+    context.subscriptions.push(targetStore);
 
     const targetModel = new TargetModel();
     const hostModel = new HostModel();
@@ -86,7 +75,6 @@ export async function activate(
     );
 
     const hostController = new HostController(hostModel, topoCli);
-    const hostHealth = new HostHealth(topoCli, hostHealthDocProvider);
     const targetController = new TargetController(
         targetModel,
         targetStore,
@@ -121,14 +109,12 @@ export async function activate(
     const containerStart = new ContainerStart(dockerCommands);
     const containerStop = new ContainerStop(dockerCommands);
     const containerDelete = new ContainerDelete(dockerCommands);
-    const targetHealth = new TargetHealth(topoCli, targetHealthDocProvider);
     const fixIssue = new FixIssue(topoCli, targetModel);
     const protocolHandler = new ProtocolHandler(topoCli);
 
     context.subscriptions.push(
         commands.register({
             hostController,
-            hostHealth,
             targetController,
             projectInit,
             deploy,
@@ -139,7 +125,6 @@ export async function activate(
             containerStart,
             containerStop,
             containerDelete,
-            targetHealth,
             fixIssue,
             projectClone,
         }),
