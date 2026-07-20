d6b55377818bb53816f9de0587395b5687d9bce7
awphi <26072111+awphi@users.noreply.github.com>
2026-07-20 10:29:03 +0100

    fetch config each time
    Signed-off-by: awphi <26072111+awphi@users.noreply.github.com>

--- src/services/config.ts
+++ src/services/config.ts
@@ -6,12 +6,9 @@ import {
 } from '../util/targetSettings';
 
 export class Config {
-    private config = vscode.workspace.getConfiguration(PACKAGE_NAME);
-
     public getTargetSettings(target: string): TargetSettings {
-        const settingsByTarget = this.config.get<unknown>(
-            CONFIG_TARGET_SETTINGS,
-        );
+        const config = vscode.workspace.getConfiguration(PACKAGE_NAME);
+        const settingsByTarget = config.get<unknown>(CONFIG_TARGET_SETTINGS);
         return resolveSettingsForTarget(target, settingsByTarget);
     }
 }
