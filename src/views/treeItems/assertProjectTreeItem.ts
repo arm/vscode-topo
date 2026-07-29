f275e763bbe0179d55d7639bab34a9deb95682d7
Federico Bozzini <federico.bozzini@arm.com>
2026-07-29 15:14:51 +0100

    Reoved jsDocs
    Signed-off-by: Federico Bozzini <federico.bozzini@arm.com>

--- src/views/treeItems/assertProjectTreeItem.ts
+++ src/views/treeItems/assertProjectTreeItem.ts
@@ -1,11 +1,6 @@
 import { logger } from '../../util/logger';
 import { ProjectTreeItem } from './projectTreeItem';
 
-/**
- * Asserts that the provided treeNode is an instance of ProjectTreeItem.
- * @param treeNode The tree node to check.
- * @throws Will throw an error if the treeNode is not a ProjectTreeItem.
- */
 export function assertProjectTreeItem(
     treeNode: unknown,
 ): asserts treeNode is ProjectTreeItem {
