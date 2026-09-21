1c4c64ee191c58113d664a916fbf4fb81fbc2fb6
Federico Bozzini <federico.bozzini@arm.com>
2026-09-21 10:41:11 +0100

    Removed unnecessary file
    Signed-off-by: Federico Bozzini <federico.bozzini@arm.com>

--- src/views/targetTreeView.ts
+++ src/views/targetTreeView.ts
@@ -11,7 +11,6 @@ import {
     TargetDescription,
     TargetHealthReport,
 } from '../services/topoCliSchema';
-import { getVisibleTargetHealthChecks } from './util/getVisibleTargetHealthChecks';
 import { LoadingTreeItem } from './treeItems/loadingTreeItem';
 import {
     compareProcessingDomains,
@@ -78,11 +77,12 @@ function getSelectedTargetChildren(
                 ];
             }
 
+            const healthChecks = [
+                ...health.data.dependencies,
+                health.data.processingDomainDriver,
+            ];
             const healthGroup = new HealthCheckGroupTreeItem(
-                loaded(
-                    getVisibleTargetHealthChecks(health.data),
-                    health.loading,
-                ),
+                loaded(healthChecks, health.loading),
             );
             const processingDomainGroup = new ProcessingDomainGroupTreeItem(
                 targetDescription,
--- src/views/util/getVisibleTargetHealthChecks.ts
+++
@@ -1,7 +0,0 @@
-import { HealthCheck, TargetHealthReport } from '../../services/topoCliSchema';
-
-export function getVisibleTargetHealthChecks(
-    health: TargetHealthReport,
-): readonly HealthCheck[] {
-    return [...health.dependencies, health.processingDomainDriver];
-}
